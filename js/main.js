108bb5c8ff4c0012260e880b075c855481df1cb2
Jin Zhou <ferryzhou@gmail.com>
2013-12-28 07:44:35 -0500

    u

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ google.load('visualization', '1', {'packages':['corechart']});
 google.setOnLoadCallback(drawChart);
   
 function drawChart() {
-  var data_api = "https://script.googleusercontent.com/macros/echo?user_content_key=eSUW619lbGFiOiC4UYlL-UyLsO4D4xdG87wN-7ZqXffZsGEPBNet6BUoO8g3dLS3Qd63PbYEaw4E23BML93TlqrzdIdPIMdAm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnLm1J9c89dvBL8aYIn17OQF3bhIv47kuKDmwUH9z5uEaC2wKV1mcpa5ky5Iz7wo1cDOmbnRGq-tk&lib=MQqC-ypIQgVILvsAPRyVbqU46mM4fGLn5";
+  var data_api = "https://script.google.com/macros/s/AKfycbzHGDaQ95wf54l7DamRvEQKbLeWCO2fhO5d3g71EPNHp_m2BY4/exec";
   var jsonData = $.ajax({
 	  url: data_api,
 	  dataType:"jsonp",

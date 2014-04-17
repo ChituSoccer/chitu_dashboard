b784047711d79fc203168b727afe45f5c40bb0c2
Jin Zhou <ferryzhou@gmail.com>
2014-04-16 21:33:51 -0400

    new structure

--- js/data.js
+++ js/data.js
@@ -37,7 +37,7 @@ function on_raw_loaded(raw) {
 
 // i-th row, j-th col, start from 0
 function get_raw_value(raw, i, j) {
-  return raw.records[i][raw.fields[j].id];
+  return raw.records[i][raw.fields[j].id].trim();
 }
 
 function get_raw_rows(raw) { return raw.records.length; }
@@ -62,6 +62,9 @@ function get_raw_col(raw, col_id) {
 
 // convert gdocs input data to a 2d array
 // omat[i][j] = raw.records[i][raw.fields[j].id]
+// the first row is game day
+// the second row is game score
+// the first column is player names
 function get_omat(raw) {
   var omat = [];
   var rows = get_raw_rows(raw);
@@ -70,6 +73,114 @@ function get_omat(raw) {
   }
   return omat;
 }
+
+// normalized raw data
+// input omat
+// return a data structure contains all information
+// { 
+//   player_count: number of players
+//   game_count:  number of games
+//   smat: side info matrix, N x M, smat(i, j) is side info of ith player and jth game
+//   player_names: player name array, N x 1, players[i] is ith player name, start from 0
+//   days: array of game day, M x 1, days[j] is jth game's day
+//   game_scores: array of scores, M x 1, scores[j] is jth game's score
+// }
+// 
+function get_ndata(dp) {
+  var N = dp.rows - 2;
+  var M = dp.cols - 1;
+  var smat = [];
+  var players = [];
+  for (var i = 0; i < N; i++) { 
+    smat[i] = dp.omat[i+2].slice(1, 1 + M); 
+    players[i] = dp.omat[i+2][0];
+  }
+  var days = dp.omat[0].slice(1, 1 + M);
+  var scores = dp.omat[1].slice(1, 1 + M);
+  return { player_count: N, game_count: M, smat: smat, player_names: players, 
+           days: days, game_scores: scores };
+}
+
+// input ndata
+// return a data structure contains high level data products
+// {
+//   win_sides: M x 1, 'W' if white wins, 'C' if color wins, 'T' if tie
+//   wlmat: N x M, win/lose matrix, wlmat[i][j] is win/lose code for ith player in jth game
+//   games: M x 1 array of game statistics structure
+//   players: N x 1 array of player statistics structure
+//   splayers: hash of sorted players, based on different score metrics
+// }
+function get_pndata(ndata) {
+  var win_sides = _.map(ndata.scores, get_win_side);
+  var wlmat = get_wlmat(ndata, win_sides);
+  var games = get_games(ndata, win_sides);
+  var players = get_players(ndata, win_sides, wlmat);
+}
+
+// return 'W', 'C' or 'T' (for tie)
+// sample input -> result 
+//   W7:C6 -> 'W'
+//   W2:C4 -> 'C'
+//   W2:C2 -> 'T'
+function get_win_side(str) {
+  if (str) {
+    var ss = str.split(':');
+    var n = {};
+    for (var i = 0; i < ss.length; i++) {
+      n[ss[i].charAt(0)] = parseInt(ss[i].substring(1));
+    }
+    if (n['W'] > n['C']) return 'W';
+    else if (n['W'] < n['C']) return 'C';
+    else return 'T';
+  } else { return 'T'; }
+}
+
+function get_win_lose_code(win_side, side) {
+  if (side) {
+    if (win_side == 'T') return 'T';
+    if (side == 'CW' || side == 'WC') return 'S';
+    if (win_side == side) return 'W';
+    return 'L';
+  } else return "";
+}
+
+// wlm[i][j] = W|L|T|"" for ith player and jth game
+function get_wlmat(ndata, win_sides) {
+  var wlm = [];
+  for (var i = 0; i < ndata.N; i++) {
+    var r = [];
+    for (var j = 0; j < ndata.M; j++) {
+      r[j] = get_win_lose_code(win_sides[j], ndata.smat[i][j]);
+    }
+    wlm[i] = r;
+  }
+  return wlm;
+}
+
+function get_games(ndata, win_sides) {
+  var games = [];
+  for (var i = 0; i < ndata.M; i++) {
+    var game = {};
+    game.id = i;
+    game.day = ndata.days[i];
+    game.score = ndata.scores[i];
+    var white_team = []; var color_team = []; var nplayers = 0;
+    for (j = 0; j < ndata.N; j++) {
+      var side = ndata.smat[j][i];
+      if (side && side != "0") nplayers++;
+      if (side == 'W') white_team.push(i);
+      else if (side == 'C') color_team.push(i);
+      else if (side == 'WC' || side == 'CW') { white_team.push(i); color_team.push(i); }
+    }
+    game.white_team = white_team; 
+    game.color_team = color_team;
+    game.nplayers = nplayers;
+    games[i] = game;
+  }
+  return games;
+}
+
+// =========================>
 
 // raw
 // fields: ids for each column
@@ -144,36 +255,9 @@ function get_game_info(dp, ind) {
   return game;
 }
 
-// return 'W', 'C' or 'T' (for tie)
-// sample input -> result 
-//   W7:C6 -> 'W'
-//   W2:C4 -> 'C'
-//   W2:C2 -> 'T'
-function get_win_side(str) {
-  if (str) {
-    var ss = str.split(':');
-    var n = {};
-    for (var i = 0; i < ss.length; i++) {
-      n[ss[i].charAt(0)] = parseInt(ss[i].substring(1));
-    }
-    if (n['W'] > n['C']) return 'W';
-    else if (n['W'] < n['C']) return 'C';
-    else return 'T';
-  } else { return 'T'; }
-}
-
 // win_sides[j] = W|C|T
 function get_win_sides(dp) {
   return _.map(dp.omat[1], get_win_side);
-}
-
-function get_win_lose_code(win_side, side) {
-  if (side) {
-    if (win_side == 'T') return 'T';
-    if (side == 'CW' || side == 'WC') return 'S';
-    if (win_side == side) return 'W';
-    return 'L';
-  } else return "";
 }
 
 
